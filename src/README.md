44b00a807672dd37bc9b6f41808fbf8dfeb86b6e
Carlos Vieira <carlos.v@sc.senai.br>
2024-08-29 16:02:31 -0300

    Estrutura base para o Projeto SOS Antiques, peguei de outro projeto que fiz no primeiro período, para utilizar a estrutura do html, css, javascript, bootstrap, fonts, e ícones.

---
+++ src/README.md
@@ -0,0 +1,16 @@
+# S.O.S Antiques
+# Instruções de utilização
+
+## Instalação do Site
+
+O site em HTML/CSS/JS é um projeto estático, logo pode ser utilizado tanto em servidores...
+
+## Histórico de versões
+
+### [0.1.0] - DD/MM/AAAA
+#### Adicionado
+- Adicionado ...
+- Para rodar o site é necessário ter as extensões 'HTML CSS Suport & Live Server' se for rodar pelo vscode.
+- Caso não tenha ou não queira rodar pelo vscode pode rodar o site abrindo o arquivo index.html no seu navegador.
+
+**Depois de Baixar o código ou clonar**
---
+++ src/js/login.js
@@ -0,0 +1,58 @@
+let button = document.querySelector('#entrar')
+
+button.addEventListener('click', entrar)
+
+
+function entrar(event) {
+    event.preventDefault();
+    let username = document.querySelector('#username');
+    let userLabel = document.querySelector('#userLabel');
+    
+    let password = document.querySelector('#password');
+    let passLabel = document.querySelector('#senhaLabel');
+
+    let msgError = document.querySelector('#msgError');
+
+    if (!username.value || !password.value) {
+        msgError.setAttribute('style', 'display: block')
+        msgError.innerHTML = 'Por favor, preencha todos os campos.';
+        return;
+    }
+
+    let listaUser = JSON.parse(localStorage.getItem('listaUser')) || []
+
+    let userValid = {
+        nome: '',
+        user: '',
+        senha: ''
+    }
+    
+    listaUser.forEach((item) => {
+        if(username.value == item.userCad && password.value == item.senhaCad) {
+            userValid = {
+                nome: item.nomeCad,
+                user: item.userCad,
+                senha: item.senhaCad
+            }
+        }
+    })
+
+    if(username.value == userValid.user && password.value == userValid.senha) {
+        setTimeout(() => {
+            window.location.href = '/pages/home.html';
+        })
+
+        let token = Math.random().toString(16).substring(2)
+        localStorage.setItem('token', token)
+
+        localStorage.setItem('userLogado', JSON.stringify(userValid))
+    } else {
+        userLabel.setAttribute('style', 'color: red')
+        username.setAttribute('style', 'border-color: red')
+        passLabel.setAttribute('style', 'color: red')
+        password.setAttribute('style', 'border-color: red')
+        msgError.setAttribute('style', 'display: block')
+        msgError.innerHTML = 'Usuário ou senha incorreto'
+        username.focus()
+    }
+}
