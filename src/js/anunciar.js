49fafa2cb3a40170365288f91205c937ce10aa73
Guilherme Campos <guilhermealc01@gmail.com>
2024-09-21 11:36:55 -0300

    Criação do método POST para cadastro de anúncios

---
+++ src/js/anunciar.js
@@ -0,0 +1,70 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const radioTroca = document.getElementById('tipo-produto--troca');
+    const radioVenda = document.getElementById('tipo-produto--venda');
+    const radioDoacao = document.getElementById('tipo-produto--doacao');
+    const inputPreco = document.getElementById('input__preco-produto');
+
+    function alternarInputPreco() {
+        if (radioTroca.checked || radioDoacao.checked) {
+            inputPreco.disabled = true;
+            inputPreco.value = '';
+        } else {
+            inputPreco.disabled = false;
+        }
+    }
+
+    alternarInputPreco();
+
+    radioTroca.addEventListener('change', alternarInputPreco);
+    radioVenda.addEventListener('change', alternarInputPreco);
+    radioDoacao.addEventListener('change', alternarInputPreco);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const formulario = document.getElementById("form");
+    const notificacao = document.getElementById("notificacao");
+
+    formulario.addEventListener('submit', event => {
+        event.preventDefault();
+
+        const formData = new FormData(formulario);
+        const data = Object.fromEntries(formData);
+        const anunciosURL = `http://localhost:3000/anuncios`;
+
+        fetch(`${anunciosURL}`, {
+            method: 'POST',
+            body: JSON.stringify({
+                "titulo-produto": data['titulo-produto'],
+                "tipo-produto": data['tipo-produto'],
+                "categoria-produto": data['categoria-produto'],
+                "condicao-produto": data['condicao-produto'],
+                "preco-produto": data['preco-produto'],
+                "descricao-produto": data['descricao-produto'],
+                "foto-produto": data['foto-produto'],
+                "termo-contato": data['termo-contato']
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => {
+            if (response.ok) {
+                notificacao.textContent = "Anúncio feito com sucesso!";
+                notificacao.classList.remove('alert-danger');
+                notificacao.classList.add('alert-success');
+            } else {
+                return response.json().then(err => { 
+                    throw new Error(err.message);
+                });
+            }
+        })
+        .catch(error => {
+            notificacao.textContent = `Erro ao fazer o anúncio: ${error.message}`;
+            notificacao.classList.remove('alert-success');
+            notificacao.classList.add('alert-danger');
+        })
+        .finally(() => {
+            notificacao.style.display = 'block';
+        });
+    });
+});
