763e7791f61140063c88cb18293fd39dfef44ca2
Carlos Vieira <carlos.v@sc.senai.br>
2024-11-26 12:49:02 -0300

    quando deslogar retornar para página de login

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 const botaoDesconectar = document.getElementById("btn-desconectar");
 
-botaoDesconectar.addEventListener("click", desconectarConta);
-
 const desconectarConta = () => {
     localStorage.removeItem('userLogado');
     window.location.href = "../pages/login.html";
 }
+
+botaoDesconectar.addEventListener("click", desconectarConta);
