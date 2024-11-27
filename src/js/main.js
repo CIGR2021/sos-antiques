d7eae9b4bd4fefccc971fec9881366dd387bc3a5
Carlos Vieira <carlos.v@sc.senai.br>
2024-11-27 12:36:53 -0300

    melhorias no main.

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ const botaoDesconectar = document.getElementById("btn-desconectar");
 
 const desconectarConta = () => {
     localStorage.removeItem('userLogado');
+    localStorage.removeItem('token');
     window.location.href = "../pages/login.html";
 }
 
